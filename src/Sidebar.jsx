71675e0205c8545aa662485dbc45d928059890de
Muthu kumar <muthukumar24081999@gmail.com>
2024-06-09 19:54:34 +0530

    first commit

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -6,13 +6,13 @@ const Sidebar = () => {
   return (
     <div className="vh-100 p-3 fixed-left">
       <h4 className='text-center text-white'>Form Navigations</h4>
-      <Nav className="flex-column">
-        <Nav.Link as={Link} to="/create-mentor" className='text-white navbar-link'>Create Mentor</Nav.Link>
-        <Nav.Link as={Link} to="/create-student" className='text-white navbar-link'>Create Student</Nav.Link>
-        <Nav.Link as={Link} to="/assign-students" className='text-white navbar-link'>Assign Students</Nav.Link>
-        <Nav.Link as={Link} to="/change-mentor" className='text-white navbar-link'>Change Mentor</Nav.Link>
-        <Nav.Link as={Link} to="/show-students" className='text-white navbar-link'>Show Students</Nav.Link>
-        <Nav.Link as={Link} to="/show-previous-mentors" className='text-white navbar-link'>Show Previous Mentors</Nav.Link>
+      <Nav className="flex-column mt-3 ps-3 gap-3">
+        <Link to="/create-mentor" className='text-white navbar-link'>Create Mentor</Link>
+        <Link to="/create-student" className='text-white navbar-link'>Create Student</Link>
+        <Link to="/assign-students" className='text-white navbar-link'>Assign Students</Link>
+        <Link to="/change-mentor" className='text-white navbar-link'>Change Mentor</Link>
+        <Link to="/show-students" className='text-white navbar-link'>Show Students</Link>
+        <Link to="/show-previous-mentors" className='text-white navbar-link'>Show Previous Mentors</Link>
       </Nav>
     </div>
   );
